98a57247b4185f8f8cc376571c6fe3a31829d210
Michael Disaro <michael.disaro@gmail.com>
2020-07-22 12:34:41 +0200

    Fixed naming conflicts with Microsoft interfaces. Refactored interface and service names. Refactored methods names.

---
+++ Interfaces/IMailer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Michaelsoft.Mailer.Interfaces
+{
+    public interface IMailer
+    {
+
+        Task SendMailAsync(Dictionary<string, string> tos,
+                            string subject,
+                            string body,
+                            Dictionary<string, string> ccs = null,
+                            Dictionary<string, string> bccs = null,
+                            Dictionary<string, Stream> attachments = null
+        );
+
+        Task SendMailUsingTemplateAsync(Dictionary<string, string> tos,
+                                         string subject,
+                                         string template,
+                                         Dictionary<string, string> parameters,
+                                         Dictionary<string, string> ccs = null,
+                                         Dictionary<string, string> bccs = null,
+                                         Dictionary<string, Stream> attachments = null
+        );
+
+    }
+}
